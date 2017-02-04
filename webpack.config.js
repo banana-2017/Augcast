dc1cfaab8a69c81138389f30a36a3479aac52e5e
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-02-03 20:49:59 -0800

    express, naming

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,16 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './js/app.js',
+    entry: './react/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+        filename: 'index.bundle.js'
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'react']
