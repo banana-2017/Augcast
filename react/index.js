9339a22008f71875d81793312bf6a8c9c853f04f
tejasbadadare <tejasbadadare@gmail.com>
2017-02-04 20:29:59 -0800

    Firebase integration working

--- react/index.js
+++ react/index.js
@@ -1,5 +1,7 @@
 const React = require ('react');
 const ReactDOM = require ('react-dom');
+var firebase = require('firebase/app');
+require('firebase/database');
 
 var Main = React.createClass ({
     render: function () {
@@ -11,5 +13,22 @@ var Main = React.createClass ({
     }
 });
 
+var conf = require('../database/credentials.json');
+
+var config = {
+    apiKey: conf.apiKey,
+    authDomain: conf.authDomain,
+    databaseURL: conf.databaseURL,
+    storageBucket: conf.storageBucket
+};
+
+// Log in the current app to firebase
+firebase.initializeApp(config);
+var database = firebase.database();
+database.ref('test/testing').set({
+    key1: "value1",
+    key2: "value2"
+});
+console.log("Writing to DB complete");
 
 ReactDOM.render (<Main/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'react']
