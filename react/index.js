906628aec53c4f20fa29f433990a125fb4d08377
tejasbadadare <tejasbadadare@gmail.com>
2017-02-04 21:01:40 -0800

    Uploading whole Firebase test object

--- react/index.js
+++ react/index.js
@@ -18,7 +18,8 @@ var database = firebase.database();
 
 // Write to the database
 database.ref('test').set({
-    working: "Live",
+    status: "Live",
+    appName: "Augcast"
 });
 console.log("Writing to DB complete");
 
@@ -27,15 +28,16 @@ var Main = React.createClass ({
         return (
             <div>
             <h1>Hello world!</h1>
-            <h2>Is Firebase working?</h2>
-            <h3>It should say "Live" on the following line.</h3>
+            <h2>Firebase "test" object: </h2>
             <h4>{this.props.dbRead}</h4>
             </div>
         )
     }
 });
 
+// Example of reading the value of the "test" JSON object from the DB
+// and then displaying it with React
 database.ref('/test').once('value').then(function(snapshot) {
-    ReactDOM.render (<Main dbRead= {snapshot.val().working} />,
+    ReactDOM.render (<Main dbRead= {JSON.stringify(snapshot.val())} />,
     document.getElementById('app'));
 });
