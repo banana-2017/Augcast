efba9a1762830c48788a482e2ab88310a11f1bcd
tejasbadadare <tejasbadadare@gmail.com>
2017-02-04 20:52:54 -0800

    Completed basic Firebase integration

--- react/index.js
+++ react/index.js
@@ -3,18 +3,8 @@ const ReactDOM = require ('react-dom');
 var firebase = require('firebase/app');
 require('firebase/database');
 
-var Main = React.createClass ({
-    render: function () {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
-});
-
+// Get credentials
 var conf = require('../database/credentials.json');
-
 var config = {
     apiKey: conf.apiKey,
     authDomain: conf.authDomain,
@@ -25,10 +15,27 @@ var config = {
 // Log in the current app to firebase
 firebase.initializeApp(config);
 var database = firebase.database();
-database.ref('test/testing').set({
-    key1: "value1",
-    key2: "value2"
+
+// Write to the database
+database.ref('test').set({
+    working: "Live",
 });
 console.log("Writing to DB complete");
 
-ReactDOM.render (<Main/>, document.getElementById('app'));
+var Main = React.createClass ({
+    render: function () {
+        return (
+            <div>
+            <h1>Hello world!</h1>
+            <h2>Is Firebase working?</h2>
+            <h3>It should say "Live" on the following line.</h3>
+            <h4>{this.props.dbRead}</h4>
+            </div>
+        )
+    }
+});
+
+database.ref('/test').once('value').then(function(snapshot) {
+    ReactDOM.render (<Main dbRead= {snapshot.val().working} />,
+    document.getElementById('app'));
+});
