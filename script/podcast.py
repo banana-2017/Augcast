27ea6118069c67efe8629433593b4efbdbb89ec5
ZhuojunChen <zhuoj.ch@gmail.com>
2017-02-19 03:36:53 -0800

    done with the lecture list in course

--- script/podcast.py
+++ script/podcast.py
@@ -9,8 +9,7 @@ lectureNumber = 0
 courseNumber = 0
 
 courseDic = {}
-lectureList = []
-
+lectureDic = {}
 
 # url and the HTML text of podcast
 urlPodcast = "https://podcast.ucsd.edu/"
@@ -40,18 +39,15 @@ for eachCourse in currentCourse.find_all('tr'):
     # refine the keyword
     courseNum = courseTitle.split('-')[0]
     courseSection = courseTitle.split('-')[-1]
-    courseID = courseNum + courseSection
+    courseID = courseNum + courseSection.replace('[', '').replace(']', '')
     courseName = "".join(courseTitle.split('-')[1:-1])
 
     # store the individual course information to the dictionary
     eachCourseDic = {}
+    lectureList = []
     eachCourseDic['name'] = courseName
     eachCourseDic['url'] = courseUrl
     eachCourseDic['professor'] = courseProf
-
-    # store the course information to the course dictionary
-    courseDic[courseID] = eachCourseDic
-
 
     ###################################### Lecture Information ###################################################
 
@@ -63,20 +59,34 @@ for eachCourse in currentCourse.find_all('tr'):
 
     lectureInfo = soupCoursePodcast.find_all('div', {"class": "lecture"})
 
+    lectureNum = 0
     for eachLecutureInfo in lectureInfo:
         eachLectureDic = {}
+
         lectureMedia = "https://podcast.ucsd.edu/Podcasts//" + eachLecutureInfo.find('span')['forfile']
         lectureDate = eachLecutureInfo.find('a').text.strip()
 
         # store the lecture information to the dictionary
         eachLectureDic['video_url'] = lectureMedia
         eachLectureDic['date'] = lectureDate
-        eachLectureDic['course'] = courseTitle
+
+        if "[" not in lectureDate:
+            lectureNum = lectureNum + 1
+
+        print lectureNum
+        lectureID = courseID + " LE" + str(lectureNum)
+
+        # TODO change lecture ID
+        lectureDic[lectureID] = eachLectureDic
 
         # store the lecture information to the lecture list
-        lectureList.append(eachLectureDic)
+        lectureList.append(lectureID)
         lectureNumber = lectureNumber + 1
 
+    # add lecture list into eachCourseDic
+    eachCourseDic["lecture"] = lectureList
+    # store the course information to the course dictionary
+    courseDic[courseID] = eachCourseDic
     courseNumber = courseNumber + 1
 
 print courseDic
@@ -89,13 +99,9 @@ with open('course.json', 'w') as outfile:
 
 with open('lecuture.json', 'w') as outfile:
     # for eachLecutureList in lectureList:
-        json.dump(lectureList, outfile)
+        json.dump(lectureDic, outfile)
         outfile.write('\n')
 
-with open('eachlecuture.json', 'w') as outfile:
-    for eachLecutureList in lectureList:
-        json.dump(eachLecutureList, outfile)
-        outfile.write('\n')
 
 print "unauthenticated course: "
 print courseNumber
