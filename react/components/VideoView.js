f50d3f57e057c5f19f2a2409ad4c0f177c51ca3b
tejasbadadare <tejasbadadare@gmail.com>
2017-02-20 20:38:12 -0800

    Add live source switching

--- react/components/VideoView.js
+++ react/components/VideoView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import VideoPlayer from './VideoPlayer';
 //import { database } from './../../database/database_init';
-//import { Button, Glyphicon } from 'react-bootstrap';
+import { Button, Glyphicon } from 'react-bootstrap';
 
 /**
     VideoView - Will contain VideoPlayer
@@ -14,22 +14,79 @@ class VideoView extends React.Component {
 
         // Initial state
         this.state = {
-
+            mediaURL: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4',
+            curSource: 1
         };
 
         // Bind all functions so they can refer to "this" correctly
         //this.togglePlay = this.togglePlay.bind(this);
+        this.handleSource1 = this.handleSource1.bind(this);
+        this.handleSource2 = this.handleSource2.bind(this);
+    }
+
+    handleSource1() {
+        this.setState({
+            curSource: 1,
+            mediaURL: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4'
+        });
+    }
+
+    handleSource2() {
+        this.setState({
+            curSource: 2,
+            mediaURL: 'http://techslides.com/demos/sample-videos/small.mp4'
+        });
     }
 
     render () {
         return (
             <div>
-                <div className="header">
-                    <h1>Video page</h1>
-                    <Link to="/">Back home</Link>
-                    <br />
+                <div className="columns">
+
+                    <div
+                        style = {{
+                            float: 'left',
+                            padding: '20px',
+                            margin: '20px',
+                            clear: 'right',
+                            height: '600px',
+                            borderStyle: 'solid',
+                            borderWidth: '2px',
+                            borderColor: 'gray'
+                        }}>
+                        <h1>Source Controller</h1>
+                        <Link to="/">Back home</Link>
+                        <br />
+
+                        <Button
+                            bsStyle="default"
+                            bsSize="large"
+                            style={{margin:'10px'}}
+                            active={this.state.curSource == 1}
+                            onClick={this.handleSource1}>
+                                Load Source 1
+                        </Button>
+                        <br />
+                        <Button
+                            bsStyle="default"
+                            bsSize="large"
+                            style={{margin:'10px'}}
+                            active={this.state.curSource == 2}
+                            onClick={this.handleSource2}>
+                                Load Source 2
+                        </Button>
+
+                    </div>
+
+                    <div style = {{
+                        padding: '20px',
+                        margin: '20px',
+
+                    }}>
+                        <VideoPlayer mediaURL={this.state.mediaURL}/>
+                    </div>
+
                 </div>
-                <VideoPlayer />
             </div>
         );
     }
