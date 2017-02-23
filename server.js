5e6813f3007c1bfdab9bf179fcbfdab83cb5fd04
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-02-22 19:05:42 -0800

    server update

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ import path from 'path';
 // eslint-disable-next-line
 import React from 'react';     // jsx rendered as React.createElement
 
-var app = express();
+let __dirname = path.resolve();
 
+var app = express();
 app.use('/css', express.static(__dirname + '/public/css/'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // morgan logs requests to the console
 app.use(morgan('dev'));
 
+console.log (__dirname);
+
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
