e1d46b6ecad1fbabb8ffac2d9b388e3d0718bc69
Alex Z. Yang <zya@ucsd.edu>
2017-02-28 17:41:11 -0800

    timestamp now overlayed on pdf

--- react/components/PDFDisplay.js
+++ react/components/PDFDisplay.js
@@ -38,6 +38,7 @@ class PDFDisplay extends React.Component {
         var PDFpages = sentinelArray.map(function(x, i){
             return (
                 <div key={'ButtonPageCombo' + i} className="pdf-page" onClick={() => {that.skipToTime(i);}}>
+                    <div className="pdf-timestamp">{i}</div>
                     <PDF
                         key={'PDFPage' + i}
                         file={that.state.file}
