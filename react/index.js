49a5f6593ce161a80ca6c3da804700d1e354be29
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-03-06 00:33:24 -0800

    fixed routing, auth block, removed unnecessary store

--- react/index.js
+++ react/index.js
@@ -17,7 +17,7 @@ import {Router, browserHistory} from 'react-router';
 let store = createStore (appReducers,
     applyMiddleware(
         thunkMiddleware,
-        createLogger
+        createLogger()
     )
 );
 
@@ -27,13 +27,31 @@ let store = createStore (appReducers,
 class Augcast extends React.Component {
     render () {
         return (
-
             <Provider store={store} >
                 <Router routes={routes} history={browserHistory}/>
             </Provider>
 
         );
     }
+}
+
+
+/**
+ * gets login state from store and redirects route
+ *
+ * nextState: current state of the router
+ * replace: triggers transition to different URL
+ * callback: continues transition
+ */
+export function authenticate (nextState, replace, transition) {
+    let {loggedIn} = store.getState();
+    console.log (loggedIn);
+    if (!loggedIn) {
+        console.log ('replacing with login');
+        replace ('/login');
+    }
+
+    transition();
 }
 
 ReactDOM.render (<Augcast/>, document.getElementById('app'));
