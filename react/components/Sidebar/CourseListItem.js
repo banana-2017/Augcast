4e29fb224717b8ded66ed26fccf61b7bd27d1776
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-03-06 21:53:49 -0800

    pinned courses pulled and highlighted

--- react/components/Sidebar/CourseListItem.js
+++ react/components/Sidebar/CourseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import FA from 'react-fontawesome';
+import {database} from '../../../database/database_init';
+import {connect} from 'react-redux';
 
 
 // render single course item
@@ -19,7 +21,21 @@ class CourseListItem extends React.Component {
     }
 
     componentWillMount () {
+        var that = this;
 
+        // get the favorites array, set state for pinned courses
+        database.ref('users/'+this.props.username+'/favorites').once('value').then(function(snapshot) {
+            let favoriteArray = snapshot.val();
+
+            // course is the index in the array
+            for (var course in favoriteArray) {
+                if (favoriteArray[course] === that.props.id) {
+                    that.setState ({
+                        pinned: true
+                    });
+                }
+            }
+        });
     }
 
     render () {
@@ -27,18 +43,32 @@ class CourseListItem extends React.Component {
         let {number, id, section, prof, selectCourse, course} = this.props;
 
         return (
-            <li className="course-item" key={id} onClick={() => {selectCourse(course);}}>
-                <div className="pin-button"><FA onClick={() => {this.pinCourse(id);}} name="star-o" size="2x"/></div>
-                <div className="course-title">
-                    <span className="course-number">{number}</span>
-                    <span className="course-section">{section}</span>
+            <li className="course-item" key={id} >
+                <div className="pin-button">
+                    <FA onClick={() => {this.pinCourse(id);}}
+                        name={(this.state.pinned)?'star':'star-o'}
+                        size="2x"
+                        className={(this.state.pinned)?'pinned':'unpinned'}/>
                 </div>
-                <div className="course-prof">{prof}</div>
-                <div className="expand-button"></div>
+                <div id="courseLabel" onClick={() => {selectCourse(course);}}>
+                    <div className="course-title">
+                        <span className="course-number">{number}</span>
+                        <span className="course-section">{section}</span>
+                    </div>
+                    <div className="course-prof">{prof}</div>
+                    <div className="expand-button"></div>
+                </div>
             </li>
         );
     }
 }
 
+function mapStateToProps (state) {
+    return {
+        username: state.username
+    };
+}
 
-export default CourseListItem;
+
+const CourseListItemContainer = connect(mapStateToProps)(CourseListItem);
+export default CourseListItemContainer;
