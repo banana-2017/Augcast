e5633a1109e6642aabb0718a0b8fe75e3898d8ee
Alex Z. Yang <zya@ucsd.edu>
2017-03-11 15:59:47 -0800

    login is not a card

--- react/components/Login.js
+++ react/components/Login.js
@@ -7,7 +7,9 @@ import {auth} from '../../database/database_init';
 
 // UI components
 import Input from 'react-toolbox/lib/input';
-import {Button} from 'react-toolbox/lib/button';
+import { Button } from 'react-toolbox/lib/button';
+import { Card } from 'react-toolbox/lib/card';
+
 
 class Login extends React.Component {
 
@@ -50,55 +52,20 @@ class Login extends React.Component {
     // TODO: needs styling
     render () {
         document.title = 'Login - Augcast';
-                // <div className="animateme">
-                //     <ul className="bg-bubbles">
-                //         <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
-                //     </ul>
-                // </div>
         return (
-            <div className="login-wrapper-none">
-                <div className="login">
-                    <Input type="email" label="Your UCSD Email" icon="email" value={this.state.email} onChange={this.emailChange} />
-                    <Input type="password" label="Password" icon="vpn_key" value={this.state.password} onChange={this.passwordChange} />
-                    <Button label="LOG IN" flat primary onClick={this.authenticate}/>
+            <div className="login-wrapper">
+                <div className="animateme">
+                    <ul className="bg-bubbles">
+                        <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                    </ul>
                 </div>
+                <Card className="login">
+                    <Input className="input-email" type="email" label="Your UCSD Email" icon="email" value={this.state.email} onChange={this.emailChange} />
+                    <Input className="input-password" type="password" label="Password" icon="vpn_key" value={this.state.password} onChange={this.passwordChange} />
+                    <Button label="LOG IN" flat primary onClick={this.authenticate}/>
+                </Card>
             </div>
         );
-        // return (
-        //     <form>
-        //         <FormGroup
-        //             controlId="email"
-        //             validationState={this.emailValidation()}>
-        //             <FormControl
-        //                 type="text"
-        //                 placeholder="@ucsd.edu"
-        //                 onChange={this.emailChange}
-        //                 value={this.state.email}
-        //                 style= {
-        //                 {   padding: '20px',
-        //                     margin: '20px',
-        //                     width: '400px'
-        //                 }}/>
-        //             <FormControl.Feedback />
-        //         </FormGroup>
-        //         <FormGroup
-        //             controlId="password">
-        //             <FormControl
-        //                 type="password"
-        //                 onChange={this.passwordChange}
-        //                 value={this.state.password}
-        //                 style= {
-        //                 {   padding: '20px',
-        //                     margin: '20px',
-        //                     width: '400px'
-        //                 }}
-        //                 placeholder="password"/>
-        //             <FormControl.Feedback />
-        //             <div id="errorMessage">{this.state.failureMessage}</div>
-        //             <Button style={{margin:'20px'}} bsStyle="success" onClick={this.authenticate}>Login</Button>
-        //         </FormGroup>
-        //     </form>
-        // );
     }
 
     // return true if email id is a valid email
