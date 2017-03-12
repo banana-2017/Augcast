914ff0ccab51403e0ac9190d2295b761f9fcd720
Alex Z. Yang <zya@ucsd.edu>
2017-03-11 18:15:31 -0800

    video buttons now minimalist

--- react/components/VideoPlayer.js
+++ react/components/VideoPlayer.js
@@ -3,6 +3,9 @@ import { database } from './../../database/database_init';
 import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
+// ui components
+import FA from 'react-fontawesome';
+
 const SKIP_VALUE = 10;
 
 /**
@@ -104,38 +107,24 @@ class VideoPlayer extends React.Component {
             <div className="video-wrapper">
                 <h2>{course.dept} {course.num} Lecture {lectureNum}, {lecture.month}/{lecture.date}</h2>
                 <div className="video-container">
-                    <video
-                        src={video_url}
-                        autoPlay
-                        width="600"
-                        muted
-                        id="basicvideo"
-                        ref="basicvideo"
-                        controls>
-                        Your browser does not support the video tag.
+                    <video src={video_url} autoPlay width="600" muted
+                           id="basicvideo" ref="basicvideo" controls>
+                            Your browser does not support the video tag.
                     </video>
                     <div className="video-button-group-container">
                         <ButtonGroup className='video-button-group'>
-                            <Button bsStyle="default"  onClick={() => {this.refs.basicvideo.currentTime -= SKIP_VALUE;}}><Glyphicon glyph="chevron-left" />Skip {SKIP_VALUE}s</Button>
-                            <Button
-                                bsStyle="primary"
-                                onClick={this.togglePlay}>
-                                    {!this.state.playing ?
-                                        <div><Glyphicon glyph="play" /> Play</div>  :
-                                        <div><Glyphicon glyph="pause" /> Pause</div>
-                                    }
-                            </Button>
-
-                            <Button bsStyle="default"  onClick={() => {this.refs.basicvideo.currentTime += SKIP_VALUE;}}>Skip {SKIP_VALUE}s <Glyphicon glyph="chevron-right" /></Button>
+                            <FA className="rewind video-control-button" name="backward"
+                                onClick={() => {this.refs.basicvideo.currentTime -= SKIP_VALUE;}}/>
+                            <FA className="toggle-play video-control-button" name={this.state.playing ? "pause" : "play"}
+                                onClick={this.togglePlay}/>
+                            <FA className="fastforward video-control-button" name="forward"
+                                onClick={() => {this.refs.basicvideo.currentTime += SKIP_VALUE;}}/>
+                            <FA className="speed-up video-control-button" name="minus"
+                                onClick={this.decreasePlaybackRate} />
+                            <span className="current-speed">Speed: {Math.abs(this.state.playbackRate).toFixed(2)}x</span>
+                            <FA className="speed-up video-control-button" name="plus"
+                                onClick={this.increasePlaybackRate} />
                         </ButtonGroup>
-
-                        <br />
-
-                        <Button style={{margin:'10px'}} bsStyle="default" bsSize="small" onClick={this.decreasePlaybackRate}><Glyphicon glyph="chevron-left" /></Button>
-                        Speed: {Math.abs(this.state.playbackRate).toFixed(2)}x
-                        <Button style={{margin:'10px'}} bsStyle="default" bsSize="small" onClick={this.increasePlaybackRate}><Glyphicon glyph="chevron-right" /></Button>
-
-                        <br />
 
                         <h4 className="main__h2">Timestamp: {this.props.timestamp}</h4>
 
