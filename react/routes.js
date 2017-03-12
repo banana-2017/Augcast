510ad6c358018e4edec3f9aed55c53bb3057cb09
Joe <checkpppp@gmail.com>
2017-03-11 19:29:05 -0800

    redux fixed

--- react/routes.js
+++ react/routes.js
@@ -15,7 +15,7 @@ import React from 'react';      // used for jsx
 
 module.exports = (
     <Route path="/" component = {App} >
-        <IndexRoute component = {InstructorPanel: onEnter={authenticate}></IndexRoute>
+        <IndexRoute component = {HomeContainer} onEnter={authenticate}></IndexRoute>
         <Route path="/login" component = {Login}/>
         <Route path="/podcastview" component={PodcastView} />
         <Route path="/upload" component = {Upload}/>
