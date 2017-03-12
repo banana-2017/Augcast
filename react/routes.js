dd669af66832863c15881ba1f401c4b06e745dcb
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-03-12 11:51:38 -0700

    removed test.js and route

--- react/routes.js
+++ react/routes.js
@@ -5,7 +5,6 @@ import Login from './components/Login';
 import PodcastView from './components/PodcastView';
 import Upload from './components/Upload';
 import PDFDisplay from './components/PDFDisplay';
-import Test from './components/Test';
 import Sidebar from './components/Sidebar/Sidebar';
 import {authenticate} from './index';
 import InstructorPanel from './components/InstructorPanel';
@@ -15,12 +14,11 @@ import React from 'react';      // used for jsx
 
 module.exports = (
     <Route path="/" component = {App} >
-        <IndexRoute component = {HomeContainer} onEnter={authenticate}></IndexRoute>
+        <IndexRoute component = {HomeContainer} /*onEnter={authenticate}*/></IndexRoute>
         <Route path="/login" component = {Login}/>
         <Route path="/podcastview" component={PodcastView} />
         <Route path="/upload" component = {Upload}/>
         <Route path="/pdf" component={PDFDisplay} />
-        <Route path="/test" component={Test} />
         <Route path="/sidebar" component={Sidebar} />
         <Route path="/pdfdisplay" component={PDFDisplay} />
         <Route path="/instructor" component={InstructorPanel} />
