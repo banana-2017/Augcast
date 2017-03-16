7205dc57b2a7311af05ea90c5006d814c1b56a74
Alan Kuo <alk029@ucsd.edu>
2017-03-15 20:21:18 -0700

    Fix issue with submitting empty question

--- react/components/ElabRequest.js
+++ react/components/ElabRequest.js
@@ -19,7 +19,7 @@ class ElabRequest extends React.Component {
             author:'',
             a_username:'',
             endorsed:'',
-            content: 'Please write your question here...',
+            content: '',
             dataRetrieved: false,
             // Used to store all elab info from database
             allRequests: undefined,
@@ -36,6 +36,8 @@ class ElabRequest extends React.Component {
         this.submitAnswer = this.submitAnswer.bind(this);
         this.removeAnswer = this.removeAnswer.bind(this);
         this.firebaseQuery = this.firebaseQuery.bind(this);
+        //this.showEntireList = this.showEntireList.bind(this);
+        //this.displayQuestion2 = this.displayQuestion2.bind(this);
     }
 
     // Grab initial data from database
@@ -96,21 +98,26 @@ class ElabRequest extends React.Component {
 
     // updating ER to database
     handleSubmit() {
-        console.log('In HandleSubmit');
-        var postData = {
-            content:this.state.content,
-            endorsed:this.state.endorsed,
-            author:this.props.username,
-        };
-        var updates = {};
-        this.updatedID = parseInt(this.updatedID)+1;
-        this.setState({updatedID: this.updatedID});
-        updates['/elaborations/' + this.props.course + '/' + this.props.lecture + '/' + this.props.timestamp + '/' + (NAME + this.updatedID)] = postData;
-        database.ref().update(updates);
-        //window.location.reload();
+        if(this.state.content!=''){
+            console.log('In HandleSubmit');
+            var postData = {
+                content:this.state.content,
+                endorsed:this.state.endorsed,
+                author:this.props.username,
+            };
+            var updates = {};
+            this.updatedID = parseInt(this.updatedID)+1;
+            this.setState({updatedID: this.updatedID});
+            updates['/elaborations/' + this.props.course + '/' + this.props.lecture + '/' + this.props.timestamp + '/' + (NAME + this.updatedID)] = postData;
+            database.ref().update(updates);
+            //window.location.reload();
 
-        // Firebase query once //
-        this.firebaseQuery();
+            // Firebase query once //
+            this.firebaseQuery();
+        }
+        else{
+            alert('You did not include any text!');
+        }
     }
 
     // Update answer to database
@@ -146,13 +153,102 @@ class ElabRequest extends React.Component {
         database.ref('/elaborations/' + this.props.course + '/' + this.props.lecture + '/' + this.props.timestamp + '/' + inputtedID).remove();
         //window.location.reload();
     }
+/*
+    renderList(){
+        console.log('Showing full list of answers');
+        var that = this;
+        database.ref('/elaborations/' + that.props.course + '/' + that.props.lecture).once('value').then(function(snapshot) {
+            var wholeList =  snapshot.val();
+            console.log('allRequest in entireList: ' + JSON.stringify(wholeList));
+            if(wholeList!=null){
+                var requestID = Object.keys(snapshot.val());
+                console.log('requestID in renderList: ' + requestID);
+                return (
+                  <div>
+                    <div>
+                      <p> INSIDE RENDERLIST </p>
+                    </div>
+                      {requestID.map(that.showEntireList.bind(this,wholeList))}
+                  </div>
+                );
+            }
+        });
+    }
 
+    showEntireList(list, currTime, index){
+        console.log('list is: ' + JSON.stringify(list));
+        console.log('currTime is: ' + currTime);
+        console.log('index is: ' + index);
+        var that = this;
+        var requestID = Object.keys(list[currTime]);
+        console.log('requestID in showEntireList: ' + JSON.stringify(requestID));
+        return (
+          <div>
+            <p> inside showEntire LIST</p>
+            {requestID.map(that.displayQuestion2.bind(this,list))}
+          </div>
+        );
+    }
+
+    // Display ER to user
+    displayQuestion2(list, requestID, index) {
+        console.log('list in 2 is: ' + JSON.stringify(list));
+        console.log('requestID in 2 is: ' + requestID);
+        console.log('index in 2 is: ' + index);
+        var allRequests = list;
+        var that = this;
+        var questions = allRequests[requestID].content;
+        var question_owner = allRequests[requestID].author;
+        var answers = allRequests[requestID].answers;
+        var answer_owner = [];
+        var keys = undefined;
+        if (answers != null && answers != undefined){
+            keys = Object.keys(answers);
+        }
+        var answers2 = [];
+        if (answers != null && answers != undefined){
+            JSON.parse(JSON.stringify(answers), (key, value) => {
+                if(key=='content'){
+                    answers2 = answers2.concat(value);
+                }
+                if(key=='a_username'){
+                    answer_owner = answer_owner.concat(value);
+                }
+            });
+        }
+        var parts = requestID.split('_');
+        that.updatedID = parts[parts.length-1];
+        return(
+          <div>
+            <div>
+              <p> inside showEntire LIST</p>
+            </div>
+            <CurrentQuestion key={requestID}
+                             elaboration={requestID}
+                             question={questions}
+                             answers={answers2}
+                             answer_owner={answer_owner}
+                             parts={parts}
+                             keys={keys}
+                             removeAnswer={this.removeAnswer}
+                             removeQuestion={this.removeQuestion}
+                             submitAnswer={this.submitAnswer}
+                             editAnswer={this.editAnswer}
+                             question_owner={question_owner}
+                             user={this.props.username}
+                             course={this.props.course}
+                             lecture={this.props.lecture} />
+          </div>
+        );
+    }
+
+    {this.state.dataRetrieved&&this.props.timestamp==undefined ? this.renderList() : <div>No questions on this lecture yet</div>}
+*/
 
     // Display ER to user
     displayQuestion(elaboration) {
         var allRequests = this.state.allRequests;
         var that = this;
-
         var questions = allRequests[elaboration].content;
         var question_owner = allRequests[elaboration].author;
         var answers = allRequests[elaboration].answers;
@@ -194,18 +290,20 @@ class ElabRequest extends React.Component {
     }
 
     render() {
+        console.log('timestamp is: ' + this.props.timestamp);
         return (
           <div className="elab-container">
               <div className="elab-list">
-                  { this.state.dataRetrieved && this.state.requestID!=undefined ?
-                      this.state.requestID.map(this.displayQuestion) : <div className="elab-empty">No questions yet</div> }
+                  { this.state.dataRetrieved && this.props.timestamp!=undefined && this.state.requestID!=undefined ? this.state.requestID.map(this.displayQuestion) : <div className="elab-empty">No questions yet</div> }
               </div>
+              {this.props.timestamp!=undefined&&
               <Question content={this.state.content}
                         handleEdit={this.handleEdit}
                         endorsed={this.state.endorsed}
                         author={this.state.author}
                         handleSubmit={this.handleSubmit}
-                        dataRetrieved={this.state.dataRetrieved}/>
+                        dataRetrieved={this.state.dataRetrieved}/>}
+
           </div>
         );
     }
