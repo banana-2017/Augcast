9579a48ae188d1853d6c5b05f8044a174f8a275e
tejasbadadare <tejasbadadare@gmail.com>
2017-03-18 17:03:56 -0700

    Fix no content uploading bug

--- labeler/labelLauncher.py
+++ labeler/labelLauncher.py
@@ -11,11 +11,6 @@ pdf_url  = sys.argv[2]
 courseID = sys.argv[3]
 lectureID = sys.argv[4]
 
-print 'media url ' + media_url
-print 'pdf url ' + pdf_url
-print 'course ID ' + courseID 
-print 'lecture ID ' + lectureID
-
 
 #test_pdf_url = "https://firebasestorage.googleapis.com/v0/b/cse110project-a890c.appspot.com/o/test1.pdf?alt=media&token=0721c873-d188-4b59-9eec-2b93d9fddf2e"
 #test_media_url = "https://podcast.ucsd.edu/Podcasts//cse100_1_wi17/cse100_1_wi17-01272017-1000.mp4"
@@ -28,10 +23,12 @@ if __name__ == '__main__':
     # error handling
     if (timestamp[0] == -3):
         print ("audio")
+        sys.stdout.flush();
         exit()
 
     if (timestamp[0] == -2):
         print ("mismatch")
+        sys.stdout.flush();
         exit()
 
     # Output json
