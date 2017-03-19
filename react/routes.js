87f486b8230af4719ebabed10339adc2f6d486ac
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-03-18 22:54:55 -0700

    routes fixed

--- react/routes.js
+++ react/routes.js
@@ -2,10 +2,6 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomeContainer from './components/Home';
 import Login from './components/Login';
-import PodcastView from './components/PodcastView';
-import Upload from './components/Upload';
-import PDFDisplay from './components/PDFDisplay';
-import Sidebar from './components/Sidebar/Sidebar';
 import {authenticate} from './index';
 import InstructorPanel from './components/InstructorPanel';
 import NotFound from './components/NotFound';
@@ -16,7 +12,6 @@ import React from 'react';      // used for jsx
 module.exports = (
     <Route path="/" component = {App} >
         <IndexRoute component = {HomeContainer} onEnter={authenticate}></IndexRoute>
-        //<IndexRoute component={HomeContainer} /* onEnter={authenticate} */></IndexRoute>
         <Route path="/login" component = {Login}/>
         <Route path="/404" component={NotFound} />
         <Route path="/instructor" component={InstructorPanel} onEnter={authenticate} />
